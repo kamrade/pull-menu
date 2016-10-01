924bd64e17cf35d169d6ff406aa1afb287ef3078
Denis Michailov <kamrad.reg@gmail.com>
2016-10-02 00:50:07 +1000

    preview

--- src/js/app.js
+++ src/js/app.js
@@ -9,9 +9,37 @@ var alertConsole = function() {
 var $grid = document.querySelector(".grid");
 var $red  = document.querySelector(".red");
 
-$grid.addEventListener("click", function(e){
-	console.log("grid");
-	e.preventDefault();
-	
+var $preview = document.querySelector('.preview');
 
+$grid.addEventListener("click", function(e){
+	if (e.target.tagName === 'IMG') {
+		var howmany = this.querySelectorAll('li').length;
+		if(howmany > 1) {
+			var removeTarget = e.target.parentNode;
+			removeTarget.parentNode.removeChild(removeTarget);
+			handler(e);
+			if(howmany === 2) {
+				var photoTitle = e.target.alt;
+				document.querySelector('.rubic .description').innerText =
+					"Selected Item = " + photoTitle;
+			}
+		}
+	}
 }, false);
+
+document.querySelector('.grid').addEventListener('mouseover', function(e){
+	if(e.target.tagName === 'IMG') {
+		var photoTitle = e.target.alt;
+		var preview = document.createElement('p');
+		preview.className = 'preview-text';
+		preview.innerText = photoTitle;
+		$preview.appendChild(preview);
+
+		e.target.addEventListener('mouseout', handler, false)
+	}
+});
+
+var handler = function(e){
+	$preview.removeChild($preview.querySelector('.preview-text'));
+	e.target.removeEventListener('mouseout', handler, false);
+}
